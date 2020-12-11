ef83d868a4cfda0968bcdca0d492f891d3946e57
ashleysheo <ashleysyheo@gmail.com>
2020-12-11 22:55:26 +0900

    change background color depending on aqi

--- script.js
+++ script.js
@@ -27,19 +27,28 @@ let countryInfo;
 
 let filterNoise = document.querySelector('.filter-noise');
 let filterColor = document.querySelector('.filter-color');
+let aqiInfo = document.querySelector('#aqi-info');
+
+let infoOn = false;
+const main = document.querySelector('.filter-noise');
+
+window.addEventListener('click', e => {
+    if (infoOn) {
+        infoOn = false;
+        aqiInfo.classList.add('fade-in');
+    } else {
+        infoOn = true;
+        aqiInfo.classList.remove('fade-in');
+    }
+});
 
 fetch('https://api.airvisual.com/v2/nearest_city?key=6f2530f7-f187-4341-b0a1-75e7744681d1')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
         const aqiData = data.data;
-        
         const city = aqiData.city;
         const country = aqiData.country;
         const aqi = aqiData.current.pollution.aqius;
-
-        // aqi info  
-        const aqiInfo = document.querySelector('#aqi-info');
 
         // particle: PM 10 
         const particleInfo = document.createElement('p');
@@ -73,34 +82,24 @@ fetch('https://api.airvisual.com/v2/nearest_city?key=6f2530f7-f187-4341-b0a1-75e
             aqiInfo.classList.add('fade-in');
         }, 4000)
 
-        let infoOn = false;
-
-        const main = document.querySelector('.filter-noise');
-
-        main.addEventListener('click', e => {
-            if (infoOn) {
-                infoOn = false;
-                aqiInfo.classList.add('fade-in');
-            } else {
-                infoOn = true;
-                aqiInfo.classList.remove('fade-in');
-            }
-        });
-
-
         if (aqi > 50 && aqi <= 100) {
-            
+            aqiInfo.classList.add('moderate');
             filterNoise.classList.add('filter-moderate--noise');
             filterColor.classList.add('filter-moderate--color')
         } else if (aqi > 100 && aqi <= 150) {
+            aqiInfo.classList.add('sensitive');
             filterNoise.classList.add('filter-sensitive--noise');
             filterColor.classList.add('filter-sensitive--color')
         } else if (aqi > 150 && aqi <= 200) {
+            aqiInfo.classList.add('unhealthy');
             filterNoise.classList.add('filter-unhealthy--noise');
             filterColor.classList.add('filter-unhealthy--color')
         } else if (aqi > 200) {
+            aqiInfo.classList.add('very-unhealthy');
             filterNoise.classList.add('filter-veryunhealthy--noise');
             filterColor.classList.add('filter-veryunhealthy--color')
+        } else {
+            aqiInfo.classList.add('good');
         }
     })
     .catch(function (error) {
@@ -143,27 +142,32 @@ button.addEventListener('click', () =>
                                     let city = aqiData.city;
                                     let country = aqiData.country;
 
+                                    if (aqi > 50 && aqi <= 100) {
+                                        aqiInfo.className = 'moderate';
+                                        filterNoise.className = 'filter-noise filter-moderate--noise';
+                                        filterColor.className = 'filter-color filter-moderate--color';
+                                    } else if (aqi > 100 && aqi <= 150) {
+                                        aqiInfo.className = 'sensitive';
+                                        filterNoise.className = 'filter-noise filter-sensitive--noise';
+                                        filterColor.className = 'filter-color filter-sensitive--color';
+                                    } else if (aqi > 150 && aqi <= 200) {
+                                        aqiInfo.className = 'unhealthy';
+                                        filterNoise.className = 'filter-noise filter-unhealthy--noise';
+                                        filterColor.className = 'filter-color filter-unhealthy--color';
+                                    } else if (aqi > 200) {
+                                        aqiInfo.className = 'very-unhealthy';
+                                        filterNoise.className = 'filter-noise filter-veryunhealthy--noise';
+                                        filterColor.className = 'filter-color filter-veryunhealthy--color';
+                                    } else {
+                                        aqiInfo.className = 'good';
+                                        filterNoise.className = 'filter-noise';
+                                        filterColor.className = 'filter-color';
+                                    }
+
                                     indexInfo.innerHTML = aqi;
                                     cityInfo.innerHTML = city;
                                     countryInfo.innerHTML = country;
 
-                                    if (aqi > 50 && aqi <= 100) {
-                                        filterNoise.className = 'filter-noise filter-moderate--noise';
-                                        filterColor.className = 'filter-color filter-moderate--color';
-                                    } else if (aqi > 100 && aqi <= 150) {
-                                        filterNoise.className = 'filter-noise filter-sensitive--noise';
-                                        filterColor.className = 'filter-color filter-sensitive--color';
-                                    } else if (aqi > 150 && aqi <= 200) {
-                                        filterNoise.className = 'filter-noise filter-unhealthy--noise';
-                                        filterColor.className = 'filter-color filter-unhealthy--color';
-                                    } else if (aqi > 200) {
-                                        filterNoise.className = 'filter-noise filter-veryunhealthy--noise';
-                                        filterColor.className = 'filter-color filter-veryunhealthy--color';
-                                    } else {
-                                        filterNoise.className = 'filter-noise';
-                                        filterColor.className = 'filter-color';
-                                    }
-                                    const aqiInfo = document.querySelector('#aqi-info');
                                     aqiInfo.classList.remove("fade-in");
                                     setTimeout(() => {
                                         aqiInfo.classList.add('fade-in');
